1c93346e2cded7370cab2e8907fb965ef56495b1
Rene Kjellerup <rk.katana.steel@gmail.com>
2022-05-17 18:52:00 -0700

    Migrated from Device ID to randomly generated UUID

--- lib/alchemy_metrics.dart
+++ lib/alchemy_metrics.dart
@@ -1,15 +1,23 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
-import 'package:platform_device_id/platform_device_id.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:uuid/uuid.dart';
+import 'package:crclib/catalog.dart';
+
+String generateGUID() {
+  final uuid = Uuid();
+  return uuid.v4();
+}
 
 void countCallback() async {
   WidgetsFlutterBinding.ensureInitialized();
-  String userID = '';
-  try {
-    userID = await PlatformDeviceId.getDeviceId ?? 'not_found';
-  } catch (e) {
-    return;
-  }
+  final prefs = await SharedPreferences.getInstance();
+
+  String userID = prefs.getString('install_id') ?? generateGUID();
+  await prefs.setString('install_id', userID);
+  String lic = Crc16().convert(utf8.encode(userID)).toString();
   Map<String, String> headers = {
     'User-Agent': 'WineHQ/1.3 App Browser',
     'Content-Type': 'application/x-www-form-urlencoded',
@@ -21,7 +29,7 @@ void countCallback() async {
   );
   Map<String, String> data = {
     'googleAcc': userID,
-    'lic': '16',
+    'lic': lic,
   };
   post(alchemy, headers: headers, body: data);
 }
